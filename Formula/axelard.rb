1ec7ec7fff3db31b90dbcb3f9646f0d0c3881849
kalid <kalid@axelar.network>
2023-03-06 12:28:00 +0000

    create formula

--- Formula/axelard.rb
+++ Formula/axelard.rb
@@ -3,7 +3,7 @@ class Axelard < Formula
   homepage "https://github.com/axelarnetwork/axelar-core"
   url "https://github.com/axelarnetwork/axelar-core/releases/download/v0.32.2/axelard-darwin-amd64-v0.32.2"
   sha256 "cac744649ea01b223a04d8916b6413f3fb504cc45585cade4f511e35fcc10f40"
-  license Apache-2.0
+  license "Apache-2.0"
 
   def install
     bin.install "axelard-darwin-amd64-v0.32.2" => "axelard"
