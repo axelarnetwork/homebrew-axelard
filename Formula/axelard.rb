1697218de9807dd34a39ed67781013d22cf4f2f6
Kalidax <kalid@axelar.network>
2023-03-06 14:24:14 +0400

    add gh action

---
+++ Formula/axelard.rb
@@ -0,0 +1,12 @@
+class Axelard < Formula
+    desc "axelard binary for darwin"
+    homepage "https://axelar.network"
+    url "https://github.com/axelarnetwork/axelar-core/releases/download/v0.32.2/axelard-darwin-amd64-v0.32.2"
+    version "0.32.2"
+    sha256 "cac744649ea01b223a04d8916b6413f3fb504cc45585cade4f511e35fcc10f40"    
+ 
+    def install
+      bin.install "axelard-darwin-amd64-v0.32.2"
+    end
+    
+end
