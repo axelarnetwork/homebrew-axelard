1b2b4981f3f80fcfbde76cb381ce3effc344f91f
Kalidax <kalid@axelar.network>
2023-03-07 14:48:53 +0400

    add arm support

--- Formula/axelard.rb
+++ Formula/axelard.rb
@@ -1,12 +1,46 @@
 class Axelard < Formula
-  desc "Axelar Core binary"
+  desc "The Axelar Network Core binary"
   homepage "https://github.com/axelarnetwork/axelar-core"
-  url "https://github.com/axelarnetwork/axelar-core/releases/download/v0.32.2/axelard-darwin-amd64-v0.32.2"
-  sha256 "cac744649ea01b223a04d8916b6413f3fb504cc45585cade4f511e35fcc10f40"
   license "Apache-2.0"
 
+  # Define the available versions and platforms
+  VERSIONS = {
+    "0.32.2" => {
+      amd64: "2303ed69ee0486814e09554449292b0b3e50f61e57248143c0d24f349df33f1d",
+      arm64: "a2abc8da15710bae0e77571dece93a8d367b1e416da193477211ed9330cdbdf4",
+    },
+  }
+
+  # Determine the platform and URL based on the system architecture
+  if Hardware::CPU.arm?
+    platform = :arm64
+    puts "ARM architecture detected"
+    puts "CPU architecture: #{Hardware::CPU.arch}"
+    puts "Is 64-bit: #{Hardware::CPU.is_64_bit?}"
+  else
+    platform = :amd64
+    puts "Intel architecture detected"
+  end
+
+  # Select the latest version if none is specified
+  version = VERSIONS.keys.last
+  puts "Selected version: #{version}"
+
+  # Retrieve the SHA256 hash for the selected version and platform
+  sha256 = VERSIONS[version][platform]
+  ohai "Warning: No SHA256 hash found for Axelard version #{version} and platform #{platform}" unless sha256
+
+  # Define the URL based on the selected version and platform
+  url "https://github.com/axelarnetwork/axelar-core/releases/download/v#{version}/axelard-darwin-#{platform}-v#{version}.zip"
+  sha256 sha256
+
+  # Install the binary
   def install
-    bin.install "axelard-darwin-amd64-v0.32.2" => "axelard"
+    if Hardware::CPU.arm?
+      bin.install "axelard-darwin-arm64-v#{version}" => "axelard"
+    else
+      bin.install "axelard-darwin-amd64-v#{version}" => "axelard"
+    end
   end
 
   test do
